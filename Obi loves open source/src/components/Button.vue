605c5f6ee4577dabbd24407c905de0d721eacea2
Akay Akin Yilmaz <akayakin.yilmaz@obi.de>
2025-06-26 14:17:55 +0200

    button

---
+++ Obi loves open source/src/components/Button.vue
@@ -0,0 +1,34 @@
+
+<template>
+
+  <button class="obi-button">
+      Obi Button
+  </button>
+
+</template>
+
+
+
+<style>
+    .obi-button {
+
+        margin-top: 5px;
+        margin-bottom: 5px;
+        background-color: rgb(245 102 0);
+        border: 0;
+        border-radius: 10px;
+        color: white;
+        padding: 10px;
+    }
+
+    .obi-button:hover {
+        background-color: black;
+        color: white;
+    }
+</style>
+
+<script>
+export default {
+
+}
+  </script>
